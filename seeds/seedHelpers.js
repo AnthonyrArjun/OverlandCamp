963d86c1f9818cfae124385e5db8a2651e375c49
AnthonyrArjun <anthonyrarjun@gmail.com>
2022-02-07 11:22:00 -0500

    initial commit

---
+++ seeds/seedHelpers.js
@@ -0,0 +1,23 @@
+module.exports.descriptors = [
+    'Mojave',
+    'Rubicon',
+    'Black Bear',
+    'Dalton',
+    'Continental',
+    'Pan-American',
+    'Smoky Mountain',
+    'Gemini',
+    'Alpine'
+]
+
+module.exports.places = [
+    'Mountains',
+    'Trail',
+    'Road',
+    'Bridges',
+    'Highway',
+    'Forest',
+    'Pass',
+    'Divide',
+]
+
