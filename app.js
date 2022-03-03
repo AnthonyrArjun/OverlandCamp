5c1b9172142f76e837b6a262a5c441428c136d34
AnthonyrArjun <anthonyrarjun@gmail.com>
2022-03-03 11:05:19 -0500

    added routes and some functionality

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./models/campground');
+const ejsMate = require('ejs-mate');
+const ExpressError = require('./utilities/ExpressError');
+const methodOverride = require('method-override')
 
-mongoose.connect('mongodb://localhost:27017/overland-camp');
+const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
+//const dbUrl = process.env.DB_URL;
+
+mongoose.connect('mongodb://localhost:27017/overland-camp'); //dbUrl
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "connection error:"));
@@ -12,22 +18,35 @@ db.once('open', () => {
     console.log('Database connected');
 });
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
+
 
 app.get('/', (req, res) => {
     res.render('home')
 });
 
-app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds})
-});
 
-app.get('/campgrounds/:id', async (req,res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/show', { campground });
-});
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 , message = 'Something went wrong' } = err;
+    if (!err.message) err.message = "Oh No. Our Website. Its broken"
+    res.status(statusCode).render('error', { err })
+})
 
 
 app.listen(2000, () => {
--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const {places, descriptors} = require('./seedHelpers');
 const Campground = require('../models/campground');
+const axios = require('axios');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+global.fetch = fetch;
 
 mongoose.connect('mongodb://localhost:27017/overland-camp');
 
@@ -11,15 +14,36 @@ db.once('open', () => {
     console.log('Database connected');
 });
 
+
+  // call unsplash and return small image
+async function seedImg() {
+    try {
+        const resp = await axios.get('https://api.unsplash.com/photos/random', {
+        params: {
+            client_id: '-ymN9aap_V5Tud9aHcG4q2fVzuTItsEs-B5rH8pMiFI',
+            collections: 'MfEeelWy2oQ',
+        },
+        })
+        return resp.data.urls.small
+    } catch (err) {
+        console.error(err)
+    }
+}
+
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++) {
+    for(let i = 0; i < 25; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20 + 10);
+
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: await seedImg(),
+            description: 'Lorem ipsum dolor sit amet. Sit dolorem inventore ut minima nisi aut unde ullam et officia omnis qui totam quis aut sint officiis. Sed dolor vero et iste velit non ipsa fugiat et perferendis reprehenderit At quaerat vero.',
+            price
         })
         await camp.save();
     }
